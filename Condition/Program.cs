a47d58013259b5ab50e8a75afe0fd3276b8f58cb
alikh <alikh@outlook.com>
2023-10-01 21:31:28 +0200

    Added condiftions

--- Condition/Program.cs
+++ Condition/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "myname";
+int age = 18;
+
+Print(name, age);
+
+static void Print(string name, int age)
+{
+    if (age < 18)
+    {
+        Console.WriteLine(name + " is not valid!");
+    }
+    else if (age >= 18 && age < 99)
+    {
+        Console.WriteLine(name + " is valid!");
+    }
+    else
+    {
+        Console.WriteLine("Out of range!");
+    }
+}
+
+// ----------- Other Samples ------------
+
+static void Sample(string name, int age)
+{
+    if (age == 10) { }
+    if (age != 10) { }
+    if (age > 10) { }
+    if (age < 10) { }
+    if (age >= 10) { }
+    if (age <= 10) { }
+    if (age > 10 && age < 20) { }
+    if (age > 10 || age == -1) { }
+
+    if (name.StartsWith("a") == true) { }
+    if (name.Length == 1) { }
+    if (name == "abc") { }
+    if (name != "abc") { }
+
+    if (age == 10 || name == "abc") { }
+    else if (age == 20) { }
+
+    if (name == "abc") { }
+    else { }
+}
