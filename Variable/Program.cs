0a8d34f19082673e34ccd240cfc6dde8e41f348f
alikh <alikh@outlook.com>
2023-10-01 17:11:28 +0200

    Added variable section

--- Variable/Program.cs
+++ Variable/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "MyName";
+int age = 12;
+double score = 80.5;
+bool isValid = true; //false
+
+Console.WriteLine("Name: " + name);
+Console.WriteLine("Age: " + age);
+Console.WriteLine("Score: " + score);
+Console.WriteLine("IsValid: " + isValid);
+
+// practice more here
+
+string firstName = "fName";
+string lastName = "lName";
+string fullName = firstName + "-" + lastName;
+
+int length = fullName.Length; //length of the fullName (how many chars!)
+string convertToString = length.ToString(); //convert to string
+
+int num1 = 1;
+int num2 = 2;
+int sum = num1 + num2;
+int mul = sum * sum;
+int calc = (num1 + num2) / mul * 10;
+
+double d1 = 1.1;
+double d2 = 2.2;
+double d3 = d1 + d2;
+
+bool b1 = true;
+bool b2 = false;
+
