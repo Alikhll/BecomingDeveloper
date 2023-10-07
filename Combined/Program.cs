6997db4de1d457007aac2811672db89caa10fe83
alikh <alikh@outlook.com>
2023-10-07 15:49:13 +0200

    combined

--- Combined/Program.cs
+++ Combined/Program.cs
@@ -1,21 +1,74 @@
-﻿using Models;
+﻿// we use "using" to import whatever package (in that case we need "Models" that has been declared inside "User.cs")
+using Models;
 
+// here we create a list of "User" and directly instantiate some values into it
 var users = new List<User>()
 {
+    //we instanciate using the constructor that has been provided and pass some information
     new User("aaa", 12),
     new User("bbb", 19),
     new User("ccc", 102),
     new User("ddd", 25),
 };
 
+// we can alway add or remove whatever into the list
 users.Add(new User("Another", 16));
+
+// we can iterate through the list regularly using "foreach"
 
 // foreach (var user in users)
 // {
 //     user.PrintIfEligible();
 // }
 
+
+// there is anothe way to iterate using "for" in this way
+// create and index called "i" from 0 and after each iteration update the index using "i++"
+// and this iteration is going to stop whenever this condition is not met anymore (i < users.Count)
 for (int i = 0; i < users.Count; i++)
 {
+    // now we use and index to get the desired user from our list
+    // and the call the "PrintIfEligible" methods afterwards
     users[i].PrintIfEligible();
 }
+
+
+
+
+
+
+
+// -----------------------------------------------
+// -------------- More Sample --------------------
+// -----------------------------------------------
+
+public class AnotherUser
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public static void Sample()
+    {
+        var users = new List<AnotherUser>()
+    {
+        //instantiate directly using their public properties
+        new AnotherUser() { Name = "aaa", Age = 12},
+        new AnotherUser() { Name = "bbb", Age = 18},
+        new AnotherUser() { Name = "ccc", Age = 55},
+        new AnotherUser() { Name = "ddd", Age = 42},
+    };
+
+        var a1 = users.Where(u => u.Name == "aaa").ToList();
+        var a2 = users.Where(u => u.Name == "aaa").First();
+        var a3 = users.Where(u => u.Name == "aaa111").FirstOrDefault();
+
+        var b1 = users.Where(u => u.Name.Contains("a")).ToList();
+        var b2 = users.Where(u => u.Name.Contains("a") && u.Age > 18).ToList();
+        var b3 = users.Where(u => (u.Name.Contains("a") && u.Age > 18) || u.Name == "bbb").ToList();
+
+        var c1 = users.Where(u => u.Name.StartsWith("a")).ToList();
+        var c2 = users.Where(u => u.Name.Length == 3).ToList();
+        var c3 = users.Where(u => u.Age > 18 && u.Age < 99).ToList();
+    }
+}
+
--- Combined/User.cs
+++ Combined/User.cs
@@ -1,16 +1,24 @@
+// we use namespace for packaging and we call it here "Models"
 namespace Models;
 
 public class User
 {
+    // we made these properties private, and they are not accesible outside of the class anymore
+    // we only have access to them inside the class
     private string Name { get; set; }
     private int Age { get; set; }
 
+    // we define a constructor and allow the consumer to provide some data
+    // and then we put it into our private properties
+    // so we strict the usage of our class, to ensure some sules for the consumer of this particular class
+    // var u = new User("abc", 18); (it should be used in that way)
     public User(string name, int age)
     {
         Name = name;
         Age = age;
     }
 
+    // a private method to be used inside the class 
     private bool IsAllowedToVote()
     {
         if (Age >= 18 && Age < 99)
@@ -21,10 +29,13 @@ public class User
         return false;
     }
 
+    // the only public method can be called from outside of the class
     public void PrintIfEligible()
     {
+        // we call this private method because we have access it only inside the class
         bool isAllowed = IsAllowedToVote();
 
+        // check the result and if it's allowed we'll print it subsequently
         if (isAllowed)
         {
             Console.WriteLine(Name + " is eligible to vote!");
