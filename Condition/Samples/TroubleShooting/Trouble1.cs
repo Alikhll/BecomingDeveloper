587a187b0580ee3b00b73c3c76abbba0d2a81377
alikh <alikh@outlook.com>
2023-10-20 20:51:43 +0200

    trouble1

---
+++ Condition/Samples/TroubleShooting/Trouble1.cs
@@ -0,0 +1,20 @@
+namespace Conditions;
+
+/*
+public partial class TroubleShootingSample
+{
+    public static void Trouble1()
+    {
+        var age = 12;
+
+        // you will get the bellow error message because you have an additonal } at line 18
+        // it's a common issue dealing with {}, (), and so on and make sure there is a proper ending for each of them
+        // error CS1022: Type or namespace definition, or end-of - file expected
+        if (age > 20)
+        {
+            Console.WriteLine("Age is Valid!");
+        }
+    }
+} // you have to remove this extra } in order to get it working
+}
+*/
